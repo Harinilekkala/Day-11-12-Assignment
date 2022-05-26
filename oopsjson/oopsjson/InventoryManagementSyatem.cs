e334bea10d7dcafe45920ec2c3cb9aab055dc8c5
Harinilekkala <harini.lekkala1997@gmail.com>
2022-05-26 20:41:08 +0530

    [Harini]: program for Json Inventory Management System regarding rice,wheat,pulses

---
+++ oopsjson/oopsjson/InventoryManagementSyatem.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopsjson
+{
+    internal class InventoryManagementSyatem
+    {
+        InventoryModelclass model = new InventoryModelclass();
+        List<InventoryModelclass> inventoryList = new List<InventoryModelclass>();
+
+        
+
+        public void group(string jFilePath)
+        {
+            using (StreamReader file = new StreamReader(jFilePath))
+            {
+                var json = file.ReadToEnd();
+                var items = JsonConvert.DeserializeObject<List<InventoryModelclass>>(json);
+                foreach (var objects in items)
+                {
+                    Console.WriteLine(objects.Name + " " + objects.price + " " + objects.weight);
+                }
+            }
+
+        }   
+    }
+}
---
+++ oopsjson/oopsjson/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+oopsjson.InventoryManagementSyatem json = new oopsjson.InventoryManagementSyatem();
+json.group(@"D:\.net\Day-11-12-Assignment\oopsjson\oopsjson\AddingJsonFile.json");
+            
